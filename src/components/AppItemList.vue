fb157b21821be273a14b86458ae56e723b3fc0a7
marcello hideyuki endo <marcelloendo@hotmail.com>
2021-04-06 19:46:38 -0300

    push1

---
+++ src/components/AppItemList.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <h5>{{ title }}<span class="badge badge-info">{{ itens.length }}</span></h5>
+    <div class="card">
+      <div class="card-body">
+        <ul class="list-group">
+          <li
+            class="list-group-item"
+            v-for="item in itens"
+            v-bind:key="item"
+          >
+            <div class="row">
+              <div class="col-md">
+                {{ item }}
+              </div>
+              <div class="col-md text-right">
+                <button class="btn btn-info">
+                  <span
+                    class="fa fa-trash"
+                    v-on:click="deleteItem(item)"
+                  ></span>
+                </button>
+              </div>
+            </div>
+          </li>
+        </ul>
+        <br />
+        <div class="input-group">
+          <input
+            class="form-control"
+            type="text"
+            placeholder="Digite o prefixo"
+            v-model="item"
+            v-on:keyup.enter="addItem(item)"
+          />
+          <div class="input-group-append">
+            <button class="btn btn-info" v-on:click="addItem(item)">
+              <span class="fa fa-plus"></span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
+</template>
+
+<script>
+export default {
+	name: "AppItemList",
+	props: ["title", "itens"],
+	data() {
+		return {
+			item: ""
+		};
+	},
+	methods: {
+		addItem(item) {
+			this.$emit("addItem", item);
+			this.item = "";
+		},
+		deleteItem(item) {
+			this.$emit("deleteItem", item);
+		}
+	}
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/DomainList.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <div id="main">
+      <div class="container">
+        <div class="row">
+          <div class="col-md">
+            <AppItemList title="Prefixo" v-bind:itens="prefixes" v-on:addItem="addPrefix" v-on:deleteItem="deletePrefix"></AppItemList>
+           </div>
+          <div class="col-md">
+            <AppItemList title="Sufixo" v-bind:itens="sufixes" v-on:addItem="addSufix" v-on:deleteItem="deleteSufix"></AppItemList>
+          </div>
+        </div>
+        <br />
+        <h5>
+          Domains <span class="badge badge-info">{{ domains.length }}</span>
+        </h5>
+        <div class="card">
+          <div class="card-body">
+            <li
+              class="list-group-item"
+              v-for="domain in domains"
+              v-bind:key="domain"
+            >
+              <div class="row">
+                <div class="col-md">
+                  {{ domain.name }}
+                </div>
+                <div class="col-md text-right">
+                  <a
+                    class="btn btn-info"
+                    v-bind:href="domain.checkout"
+                    target="_blank"
+                  >
+                    <span class="fa fa-search"> </span>
+                  </a>
+                </div>
+              </div>
+            </li>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+import AppItemList from "./AppItemList";
+
+export default {
+	name: "App",
+	components: {
+		AppItemList,
+	},
+	data: function () {
+		return {
+			prefix: "",
+			sufix: "",
+			prefixes: ["Air", "Jet", "Flight"],
+			sufixes: ["Hub", "Station", "Mart"],
+		};
+	},
+	methods: {
+		addPrefix(prefix) {
+			this.prefixes.push(prefix);
+		},
+		deletePrefix(prefix) {
+			this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
+			this.generate();
+		},
+		addSufix(sufix) {
+			this.sufixes.push(sufix);
+		},
+		deleteSufix(sufix) {
+			this.sufixes.splice(this.sufixes.indexOf(sufix), 1);
+		},
+	},
+	computed: {
+		domains() {
+			const domains = [];
+			for (const prefix of this.prefixes) {
+				for (const sufix of this.sufixes) {
+					const name = prefix + sufix;
+					const url = name.toLowerCase();
+					const checkout = `https://www.google.com/search?q=${url}`;
+					domains.push({
+						name,
+						checkout,
+					});
+				}
+			}
+			return domains;
+		},
+	},
+};
+</script>
+
+<style>
+
+</style>
